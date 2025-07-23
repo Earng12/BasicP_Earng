571aef25338c92843aacbea6bc7c0b33c73a488a
Student <Student@Train11-2-PC347.control.ad.sit.kmutt.ac.th>
2025-07-23 12:25:13 +0700

    อัพไฟล์

---
+++ Class3.py
@@ -0,0 +1,11 @@
+# x = int(input("วนกี่รอบ : "))
+# sum = 0
+# for i in range(1,x+1):
+#     sum += i
+#     print("วนรอบที่", i, "ผลรวมคือ", sum)
+
+while True:
+    inp = int(input("กรอกตัวเลข :"))
+    if inp == 7:
+        break
+
