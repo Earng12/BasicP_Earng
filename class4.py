77734376d0783ce9cb8ad5abf167d3a1956bab18
Student <Student@Train11-2-PC347.control.ad.sit.kmutt.ac.th>
2025-07-24 12:37:52 +0700

    เพิ่ม 4

---
+++ class4.py
@@ -0,0 +1,49 @@
+# def hello():
+#     print("hello world")
+
+# hello()
+
+
+# def add(a,b):
+#     print(a+b)
+# add(1,2)
+# add(29,90)
+# add(39,89)
+
+# x = 15
+# y = 89
+
+# add(x,y)
+
+# def introduction(name):
+#     print("my name is", name)
+#     tellage()
+
+# def tellage():
+#     age = input("Enter your age :")
+#     print("อายุของคุนคือ: ", age)
+# introduction("Earng")
+
+# def spam(x):
+#     for i in range(0,2):
+#         print(x)
+# spam("fj")
+
+# x = ["earng","eiei",2,7]
+
+# for i in x:
+
+# 
+
+students = [
+    {
+    "name": "earng",
+     "id": 1,
+    "score": 44
+    },
+    {"name": "eiei","id": 2, "score": 55},
+]
+for x in students:
+    if x["name"] == "eiei":
+        x["score"] = 0
+print(students)
